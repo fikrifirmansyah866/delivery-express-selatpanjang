52a20ab0d057c8acbafd2591dc141cd263c929e5
fikrifirmansyah866 <alfatihrafiahmad081@gmail.com>
2026-03-18 06:32:59 +0700

    Update script.js
    Update script.js
    
    Signed-off-by: fikrifirmansyah866 <alfatihrafiahmad081@gmail.com>

--- script.js
+++ script.js
@@ -3,14 +3,39 @@ function login() {
   document.getElementById('homePage').style.display = 'block';
 }
 
+function logout() {
+  document.getElementById('homePage').style.display = 'none';
+  document.getElementById('loginPage').style.display = 'block';
+}
+
 function showOrder() {
   document.getElementById('homePage').style.display = 'none';
   document.getElementById('orderPage').style.display = 'block';
+}
+
+function backHome() {
+  document.getElementById('orderPage').style.display = 'none';
+  document.getElementById('homePage').style.display = 'block';
 }
 
 function order() {
   let pickup = document.getElementById('pickup').value;
   let dropoff = document.getElementById('dropoff').value;
 
-  alert('Order dibuat dari ' + pickup + ' ke ' + dropoff);
-                                       }
+  if (pickup === "" || dropoff === "") {
+    alert("Isi lokasi dulu!");
+    return;
+  }
+
+  // simulasi jarak & harga
+  let jarak = Math.floor(Math.random() * 10) + 1; // 1-10 km
+  let harga = jarak * 3000;
+
+  alert(
+    "Order Berhasil!\n\n" +
+    "Jarak: " + jarak + " km\n" +
+    "Harga: Rp " + harga
+  );
+
+  backHome();
+}
